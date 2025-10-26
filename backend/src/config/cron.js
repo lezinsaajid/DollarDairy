26eaec0ea9d378f216903bb7f16ed59d0e4ee5ac
lezinsaajid <lezinsajid@gmail.com>
2025-10-27 00:16:02 +0530

    cron adjusted again

--- backend/src/config/cron.js
+++ backend/src/config/cron.js
@@ -1,7 +1,7 @@
 const { CronJob } = require("cron");
 const https = require("https");
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
     https
         .get(process.env.API_URL, (res) => {
             if (res.statusCode === 200) console.log("GET request sent successfully");
